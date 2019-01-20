6460e762ebbc48e159e4595f2a40075ffe52e063
Ahmed Nasr <ahmedhosna95@gmail.com>
2019-01-20 07:53:46 +0200

    Completed the reavel-on-scroll task.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,7 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 const mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
